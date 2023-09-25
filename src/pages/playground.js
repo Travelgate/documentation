950cde04e3266af0c280fcd30f3905afd3d29199
Calin Rada <rada.calin@gmail.com>
2023-09-25 15:33:50 +0300

    Fixed SSR for GraphiQL

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import 'graphiql/graphiql.css';
-import { createGraphiQLFetcher } from '@graphiql/toolkit';
+import { createGraphiQLFetcher } from '@graphiql/create-fetcher';
 import { GraphiQL } from 'graphiql';
-const fetcher = createGraphiQLFetcher({ url: 'https://api.travelgatex.com/' });
+import useIsBrowser from '@docusaurus/useIsBrowser';
 
 const Explorer = () => {
     const { siteConfig } = useDocusaurusContext();
+    const isBrowser = useIsBrowser();
+    let fetcher = null;
+
+    useEffect(() => {
+        if (isBrowser) {
+            fetcher = createGraphiQLFetcher({ url: 'https://api.travelgatex.com/' });
+        }
+    }, []);
+
     return (
         <Layout
             title={siteConfig.title}
-            description="Slerp GraphQL Explorer">
-            <GraphiQL fetcher={fetcher} />
-
+            description="GraphQL Explorer">
+            {isBrowser && fetcher && (
+                <BrowserOnly fallback={<div>Loading...</div>}>
+                    {() => {
+                        return <GraphiQL fetcher={fetcher} headers={'{"Authorization": "Bearer YOUR_BEARER_HERE"}'} />
+                        //return <></>
+                    }}
+                </BrowserOnly>
+            )}
         </Layout>
     );
 }

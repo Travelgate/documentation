24c0e4b9edb8e96e73cde4ce595b75d3e9d9fdc4
Calin Rada <rada.calin@gmail.com>
2023-10-30 16:08:25 +0200

    Added initial rest playground component

---
+++ src/components/RestPlayground.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+function RestPlayground() {
+    const [endpoint, setEndpoint] = useState('https://api.travelgate.com');
+    const [method, setMethod] = useState('POST');
+    const [requestData, setRequestData] = useState('');
+    const [response, setResponse] = useState('');
+
+    const sendRequest = async () => {
+        try {
+            const res = await fetch(endpoint, {
+                method: method,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: method === 'GET' ? null : requestData
+            });
+            const data = await res.json();
+            setResponse(JSON.stringify(data, null, 2));
+        } catch (error) {
+            setResponse('Error: ' + error.toString());
+        }
+    };
+
+    return (
+        <div>
+            <div>
+                <select id="method" value={method} onChange={(e) => setMethod(e.target.value)}>
+                    <option>GET</option>
+                    <option>POST</option>
+                    {/* Add other methods as necessary */}
+                </select>
+                <input
+                    id="endpoint"
+                    type="text"
+                    placeholder="https://api.travelgate.com"
+                    value={endpoint}
+                    onChange={(e) => setEndpoint(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="requestData">Request Data (for POST, PUT, etc.):</label>
+                <textarea
+                    id="requestData"
+                    value={requestData}
+                    onChange={(e) => setRequestData(e.target.value)}
+                ></textarea>
+            </div>
+            <button onClick={sendRequest}>Send Request</button>
+
+            <div>
+                <label>Response:</label>
+                <pre>{response}</pre>
+            </div>
+        </div>
+    );
+}
+
+export default RestPlayground;

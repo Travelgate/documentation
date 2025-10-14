c253e3a21700ffcb9212dd07a926b0c2a616f9b5
Camila Pérez <camperezarevalo@hotmail.com>
2025-10-14 12:18:21 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   url: 'https://docs.travelgate.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/documentation',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -89,7 +89,7 @@ const config = {
       {
         schema: "./schemas/filtered-schema.graphql",
         rootPath: "./api",
-        baseURL: "/",
+        baseURL: "/documentation",
         linkRoot: "/api",
         loaders: {
           GraphQLFileLoader: "@graphql-tools/graphql-file-loader", // local file schema

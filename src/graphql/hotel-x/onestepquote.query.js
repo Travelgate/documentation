e223513a0e725b14725dc8d6757bb18773457f56
Camila Pérez <camila.perez@travelgate.com>
2025-10-27 15:35:46 +0100

    Update onestepquote and onestepbook query example (#593)
    * Update onestepquote and onestepbook query example
    
    * Update onestepquote and onestepbook query example
    
    ---------
    
    Co-authored-by: Marcos <bsmarcosj@users.noreply.github.com>

--- src/graphql/hotel-x/onestepquote.query.js
+++ src/graphql/hotel-x/onestepquote.query.js
@@ -50,61 +50,65 @@ export const oneStepQuoteQuery =
 
 export const oneStepQuoteVariables = 
 `{
-	"input": {
-		"language": "es",
-		"market": "ES",
-		"nationality": "ES",
-		"checkIn": "2024-10-28",
-		"checkOut": "2024-10-29",
-		"hotel": "1",
-		"board": "1",
-		"price": {
-			"currency": "EUR",
-			"binding": false,
-			"net": 146,
-			"gross": 146
-		},
-		"ratePlan": "BAR",
-		"rooms": [
-			{
-				"occupancyRefId": 1,
-				"code": "2269",
-				"paxes": []
-			}
-		],
-		"rateRulesExtended": [
-			{
-				"type": "PACKAGE"
-			},
-			{
-				"type": "NON_REFUNDABLE"
-			}
-		],
-		"occupancies": [
-			{
-				"paxes": [
-					{
-						"age": 30
-					},
-					{
-						"age": 30
-					}
-				]
-			}
-		],
-		"cancelPolicy": {
-			"refundable": false,
-			"cancelPenalties": []
-		}
-	},
-	"settings": {
-		"testMode": true,
-		"client": "client_demo",
-		"context": "HOTELTEST",
-		"timeout": 20000
-	},
-	"filter": {
-		"access": "2",
-		"excludeComparisonFields": "SURCHARGES"
-	}
+    "input" : {
+        "language" : "es",
+        "market" : "ES",
+        "nationality" : "ES",
+        "checkIn" : "2027-05-28",
+        "checkOut" : "2027-05-29",
+        "hotel" : "1",
+        "board" : "14",
+        "price" : {
+            "currency" : "EUR",
+            "binding" : false,
+            "net" : 96.04,
+            "gross" : 98
+        },
+        "ratePlan" : "BAR",
+        "rooms" : [
+            {
+                "occupancyRefId" : 1,
+                "code" : "2269",
+                "paxes" : [
+                ]
+            }
+        ],
+        "rateRulesExtended" : [
+            {
+                "type" : "PACKAGE"
+            },
+            {
+                "type" : "NON_REFUNDABLE"
+            }
+        ],
+        "occupancies" : [
+            {
+                "paxes" : [
+                    {
+                        "age" : 30
+                    },
+                    {
+                        "age" : 30
+                    }
+                ]
+            }
+        ],
+        "cancelPolicy" : {
+            "refundable" : false,
+            "cancelPenalties" : [
+            ]
+        }
+    },
+    "settings" : {
+        "testMode" : true,
+        "client" : "client_demo",
+        "context" : "HOTELTEST",
+        "timeout" : 20000
+    },
+    "filter" : {
+        "access" : "2",
+        "excludeComparisonFields" : [
+            "SURCHARGES"
+        ]
+    }
 }`

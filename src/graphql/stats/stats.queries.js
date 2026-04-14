46ac3af4c48356bfde5c1a1660dec033274a0a64
carolina.martinez <carolina.martinez@travelgate.com>
2026-04-14 16:53:13 +0200

    fix: add interactive playground

---
+++ src/graphql/stats/stats.queries.js
@@ -0,0 +1,64 @@
+export const getAggregationStatsQuery = `query getAggregatedStats($where: StatsAggregationConnectionWhereInput!, $groupBy: StatsAggregationConnectionGroupByInput!) {
+  stats {
+    getAggregationStats(where: $where, groupBy: $groupBy) {
+      totalCount
+      edges {
+        node {
+          aggregationData {
+            hits
+            time
+            totalTime
+            operationType
+            seller { code }
+            buyer { code }
+          }
+        }
+      }
+    }
+  }
+}`;
+
+export const getAggregationStatsVariables = `{
+  "where": {
+    "mode": "BUYER",
+    "buyer_in": ["BUYER_CODE"],
+    "operationType_in": ["SEARCH"],
+    "timeRange": {
+      "absolute": {
+        "from": "2026-04-14 00:00:00",
+        "to": "2026-04-14 12:00:00"
+      }
+    }
+  },
+  "groupBy": {
+    "data": ["OPERATION"]
+  }
+}`;
+
+export const getRealTimeStatsQuery = `query getRealTimeStats($where: StatsRealTimeRangeWhereInput!) {
+  stats {
+    getRealTimeRangeStats(where: $where) {
+      realTimeRangeData {
+        from
+        to
+      }
+      adviseMessage {
+        code
+        description
+        level
+      }
+    }
+  }
+}`;
+
+export const getRealTimeStatsVariables = `{
+  "where": {
+    "timeRange": {
+      "absolute": {
+        "from": "2026-04-14 15:00:00",
+        "to": "2026-04-14 16:00:00"
+      }
+    },
+    "groupByTime": "MINUTE"
+  }
+}`;

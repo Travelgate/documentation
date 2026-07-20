c4317b6d18b4f9da07f71bc25257ee731cce056e
Raquel Rodriguez Diaz <raquel.rodriguez@travelgate.com>
2026-07-20 13:05:09 +0200

    refactor: remove login functionality and clean up navbar component

--- src/theme/Navbar/Content/index.js
+++ src/theme/Navbar/Content/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {useThemeConfig, ErrorCauseBoundary} from '@docusaurus/theme-common';
 import {
   splitNavbarItems,
@@ -10,10 +10,6 @@ import SearchBar from '@theme/SearchBar';
 import NavbarMobileSidebarToggle from '@theme/Navbar/MobileSidebar/Toggle';
 import NavbarLogo from '@theme/Navbar/Logo';
 import NavbarSearch from '@theme/Navbar/Search';
-import { useAuth0 } from "@auth0/auth0-react";
-import {useQuery, gql} from "@apollo/client";
-import {allClientsListQuery} from "../../../graphql/admin/allclients.query";
-
 import styles from './styles.module.css';
 
 function useNavbarItems() {
@@ -51,46 +47,11 @@ function NavbarContentLayout({left, right}) {
   );
 }
 
-const LogoutAndName = () => {
-    const { logout, getIdTokenClaims } = useAuth0();
-    const [email, setEmail] = React.useState(null);
-    const { data, loading, error } = useQuery(gql(allClientsListQuery));
-
-    useEffect(() => {
-        (async () => {
-            try {
-                const token = await getIdTokenClaims();
-                localStorage.setItem('token', token.__raw);
-                setEmail(token.email)
-            } catch (e) {
-                console.error(e);
-            }
-        })();
-    }, []);
-
-    useEffect(() => {
-        if (!loading && !error && data) {
-            localStorage.setItem('client', data?.admin?.allClients?.edges[0]?.node?.clientData?.name);
-        }
-    }, [loading, error, data]);
-
-    return (
-        <>
-            <NavbarItem label={`Logout ${email}`} position="right" to={`#`} onClick={() => logout({
-                logoutParams: {
-                    returnTo: window.location.origin
-                }
-            })} />
-        </>
-    )
-}
-
 export default function NavbarContent() {
   const mobileSidebar = useNavbarMobileSidebar();
   const items = useNavbarItems();
   const [leftItems, rightItems] = splitNavbarItems(items);
   const searchBarItem = items.find((item) => item.type === 'search');
-  const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
 
 
   return (
@@ -108,11 +69,6 @@ export default function NavbarContent() {
         // Ask the user to add the respective navbar items => more flexible
         <>
           <NavbarItems items={rightItems} />
-            {isAuthenticated ? (
-                <LogoutAndName />
-            ) : (
-                <NavbarItem label={`Login`} position="right" to={`#`} onClick={() => loginWithRedirect()} />
-            )}
 
           {/*<NavbarColorModeToggle className={styles.colorModeToggle} />*/}
           {!searchBarItem && (
